c57f8288c7edfbc8dbe7f2156071ae22bc4b9b61
linnlatt <linn.latt.yamone.qz@gmail.com>
2025-10-31 20:36:03 +0700

    Completed & Perfect MenuPage <3<3

--- src/main/java/se233/project2/Launcher.java
+++ src/main/java/se233/project2/Launcher.java
@@ -12,24 +12,33 @@ public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) {
-//        GameStage gameStage = new GameStage();
-//        GameLoop gameLoop = new GameLoop(gameStage);
-//        Scene scene = new Scene(gameStage, gameStage.WIDTH, gameStage.HEIGHT);
-//        scene.setOnKeyPressed(event-> gameStage.getKeys().add(event.getCode()));
-//        scene.setOnKeyReleased(event ->  gameStage.getKeys().remove(event.getCode()));
-//        primaryStage.setTitle("2D Platformer");
-//        primaryStage.setScene(scene);
+//
+//    @Override
+//    public void start(Stage primaryStage) {
+////        GameStage gameStage = new GameStage();
+////        GameLoop gameLoop = new GameLoop(gameStage);
+////        Scene scene = new Scene(gameStage, gameStage.WIDTH, gameStage.HEIGHT);
+////        scene.setOnKeyPressed(event-> gameStage.getKeys().add(event.getCode()));
+////        scene.setOnKeyReleased(event ->  gameStage.getKeys().remove(event.getCode()));
+////        primaryStage.setTitle("2D Platformer");
+////        primaryStage.setScene(scene);
+////        primaryStage.show();
+////        gameStage.requestFocus();
+////        gameLoop.start();     }
+//        StageManager stageManager = new StageManager(primaryStage);
+//        stageManager.loadStage(1);
+//        primaryStage.setTitle("Contra Game");
 //        primaryStage.show();
-//        gameStage.requestFocus();
-//        gameLoop.start();     }
-        StageManager stageManager = new StageManager(primaryStage);
-        stageManager.loadStage(1);
-        primaryStage.setTitle("Contra Game");
-        primaryStage.show();
+//
+//
+//    }
 
+@Override
+public void start(Stage primaryStage) {
+    StageManager stageManager = new StageManager(primaryStage);
+    stageManager.loadStage(0); // start from menu now
+    primaryStage.setTitle("Contra Game");
+    primaryStage.show();
+}
 
-    }
 }
--- src/main/java/se233/project2/controller/StageManager.java
+++ src/main/java/se233/project2/controller/StageManager.java
@@ -3,6 +3,7 @@ package se233.project2.controller;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import se233.project2.view.GameStage;
+import se233.project2.view.MenuPage;
 
 public class StageManager {
     private int currentStage = 1;
@@ -14,20 +15,24 @@ public class StageManager {
     }
 
     public void loadStage(int num) {
-        GameStage stage = switch (num) {
-            case 1 -> GameStage.stage1();
+        switch (num) {
+            case 0: // Menu page
+                MenuPage menuPage = new MenuPage(this);
+                scene = new Scene(menuPage, GameStage.WIDTH, GameStage.HEIGHT);
+                window.setScene(scene);
+                break;
 
-            default -> null;
-        };
+            case 1: // Game stage
+                GameStage stage = GameStage.stage1();
+                scene = new Scene(stage, GameStage.WIDTH, GameStage.HEIGHT);
+                scene.setOnKeyPressed(event -> stage.getKeys().add(event.getCode()));
+                scene.setOnKeyReleased(event -> stage.getKeys().remove(event.getCode()));
+                window.setScene(scene);
+                new GameLoop(stage).start();
+                break;
 
-        if (stage != null) {
-            scene = new Scene(stage, GameStage.WIDTH, GameStage.HEIGHT);
-            scene.setOnKeyPressed(event -> stage.getKeys().add(event.getCode()));
-            scene.setOnKeyReleased(event -> stage.getKeys().remove(event.getCode()));
-            window.setScene(scene);
-
-            new GameLoop(stage).start();
+            default:
+                throw new IllegalArgumentException("Invalid stage number: " + num);
         }
     }
 }
-
