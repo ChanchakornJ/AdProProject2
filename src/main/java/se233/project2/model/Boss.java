cc41102437a89011337c87df5b1e7ff1a0df3036
linnlatt <linn.latt.yamone.qz@gmail.com>
2025-11-02 20:06:15 +0700

    Adjust size of explosion effect

--- src/main/java/se233/project2/model/Boss.java
+++ src/main/java/se233/project2/model/Boss.java
@@ -210,11 +210,12 @@ public class Boss extends Pane {
         explosion.setViewport(frames[0]);
 
         explosion.setPreserveRatio(true);
-        explosion.setFitWidth(frames[0].getWidth() * 30);   // 2 เท่าของเฟรม
-        explosion.setFitHeight(frames[0].getHeight() * 30);
+        explosion.setFitWidth(frames[0].getWidth() * 12);   // 2 เท่าของเฟรม
+        explosion.setFitHeight(frames[0].getHeight() * 12);
 
         explosion.setX(box.getMinX() + (box.getWidth() / 2) - (explosion.getFitWidth() / 2));
         explosion.setY(box.getMinY() + (box.getHeight() / 2) - (explosion.getFitHeight() / 2));
+//        explosion.setY(box.getMinY() - 50); // ยกขึ้น 50 px จากขอบบนของ box
 
         Pane root = (Pane) getParent();
         root.getChildren().add(explosion);
