127be1c5e36f36a936c968c9e8189c3127cb0703
linnlatt <linn.latt.yamone.qz@gmail.com>
2025-11-02 12:59:27 +0700

    add transition

--- src/main/java/se233/project2/controller/StageManager.java
+++ src/main/java/se233/project2/controller/StageManager.java
@@ -20,47 +20,32 @@ public class StageManager {
                 MenuPage menuPage = new MenuPage(this);
                 scene = new Scene(menuPage, GameStage.WIDTH, GameStage.HEIGHT);
                 window.setScene(scene);
-                window.setResizable(false);
-                window.sizeToScene();
-
                 break;
 
             case 1: // Game stage
                 GameStage stage1 = GameStage.stage1();
-                stage1.getGameCharacterList().get(0).setStageManager(this);
                 scene = new Scene(stage1, GameStage.WIDTH, GameStage.HEIGHT);
                 scene.setOnKeyPressed(event -> stage1.getKeys().add(event.getCode()));
                 scene.setOnKeyReleased(event -> stage1.getKeys().remove(event.getCode()));
                 window.setScene(scene);
-                window.setResizable(false);
-                window.sizeToScene();
-
                 new GameLoop(stage1).start();
                 break;
 
             case 2: // Game stage
                 GameStage stage2 = GameStage.stage2();
-                stage2.getGameCharacterList().get(0).setStageManager(this);
                 scene = new Scene(stage2, GameStage.WIDTH, GameStage.HEIGHT);
                 scene.setOnKeyPressed(event -> stage2.getKeys().add(event.getCode()));
                 scene.setOnKeyReleased(event -> stage2.getKeys().remove(event.getCode()));
                 window.setScene(scene);
-                window.setResizable(false);
-                window.sizeToScene();
-
                 new GameLoop(stage2).start();
                 break;
 
             case 3: // Game stage
                 GameStage stage3 = GameStage.stage3();
-                stage3.getGameCharacterList().get(0).setStageManager(this);
                 scene = new Scene(stage3, GameStage.WIDTH, GameStage.HEIGHT);
                 scene.setOnKeyPressed(event -> stage3.getKeys().add(event.getCode()));
                 scene.setOnKeyReleased(event -> stage3.getKeys().remove(event.getCode()));
                 window.setScene(scene);
-                window.setResizable(false);
-                window.sizeToScene();
-
                 new GameLoop(stage3).start();
                 break;
 
@@ -68,10 +53,8 @@ public class StageManager {
                 throw new IllegalArgumentException("Invalid stage number: " + num);
         }
     }
+
     public int getCurrentStage() {
         return currentStage;
     }
-
-
-
 }
---
+++ src/main/java/se233/project2/view/GameOverPage.java
@@ -0,0 +1,52 @@
+package se233.project2.view;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import se233.project2.Launcher;
+import se233.project2.controller.StageManager;
+
+public class GameOverPage extends StackPane {
+    private Font arcadeFont;
+
+    public GameOverPage(StageManager stageManager) {
+
+        setPrefSize(GameStage.WIDTH, GameStage.HEIGHT);
+
+        arcadeFont = Font.loadFont(getClass().getResourceAsStream("/se233/project2/assets/PressStart2P-Regular.ttf"), 24);
+        if (arcadeFont == null) {
+            arcadeFont = Font.font("Impact", 24);
+        }
+        // Background
+        Image bgImage;
+        try {
+            bgImage = new Image(Launcher.class.getResourceAsStream("assets/Stage1.png"));
+        } catch (Exception e) {
+            bgImage = null;
+        }
+
+        if (bgImage != null) {
+            ImageView bg = new ImageView(bgImage);
+            bg.setFitWidth(GameStage.WIDTH);
+            bg.setFitHeight(GameStage.HEIGHT);
+            bg.setStyle("-fx-background-color: #222222;");
+            getChildren().add(bg);
+        } else {
+            setStyle("-fx-background-color: linear-gradient(to bottom, #000000, #333333);");
+        }
+
+        StackPane overlay = new StackPane();
+        overlay.setStyle("-fx-background-color: rgba(0, 0, 0, 0.6);"); // 60% black overlay
+        overlay.setPrefSize(GameStage.WIDTH, GameStage.HEIGHT);
+        getChildren().add(overlay);
+
+        // Title
+        Text title = new Text("GAME OVER!");
+        title.setFont(Font.font(arcadeFont.getFamily(), 48));
+        title.setFill(Color.ORANGE);
+
+    }
+}
